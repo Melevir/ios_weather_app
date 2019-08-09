efd6af66ce6059f215172183af6830d3bdad689e
Илья Лебедев <i.lebedev@bestdoctor.ru>
2019-08-09 21:55:37 +0300

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Ilya Lebedev on 09/08/2019.
+//  Copyright © 2019 Ilya Lebedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeatherTests/WeatherTests.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Ilya Lebedev on 09/08/2019.
+//  Copyright © 2019 Ilya Lebedev. All rights reserved.
+//
+
+import XCTest
+@testable import Weather
+
+class WeatherTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
