f8b2674c9e272f225efeceb79b025de0180b90a5
Илья Лебедев <i.lebedev@bestdoctor.ru>
2019-08-10 17:03:03 +0300

    Add alert on network problems

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,26 +9,14 @@
 import UIKit
 import Alamofire
 
-struct WeatherResponse: Decodable {
-  struct MainWeather: Decodable {
-    let temp: Double
-  }
-  struct Weather: Decodable {
-    let main: String
-  }
-  let coord: [String: Double]
-  let main: MainWeather
-  let weather: [Weather]
-}
-
 class ViewController: UIViewController {
-  @IBOutlet weak var temperatureTextField: UITextField!
-  @IBOutlet weak var cityNameTextField: UITextField!
-  @IBOutlet weak var weatherIconTextField: UITextField!
-  @IBOutlet weak var scrollView: UIScrollView!
+  @IBOutlet private weak var temperatureTextField: UITextField!
+  @IBOutlet private weak var cityNameTextField: UITextField!
+  @IBOutlet private weak var weatherIconTextField: UITextField!
+  @IBOutlet private weak var scrollView: UIScrollView!
 
-  let cities = ["Moscow", "London", "Paris", "New-York"]
-  var currentCity: String?
+  private let cities = ["Moscow", "London", "Paris", "New York"]
+  private var currentCity: String?
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -40,25 +28,29 @@ class ViewController: UIViewController {
     scrollView.refreshControl = refreshControl
   }
 
-  @objc func updateWeatherData(refreshControl: UIRefreshControl) {
+  @objc private func updateWeatherData(refreshControl: UIRefreshControl) {
     fetchAndShowDataFor(currentCity!)
     refreshControl.endRefreshing()
   }
 
-  @IBAction func swipeAction(_ sender: UISwipeGestureRecognizer) {
+  @IBAction private func swipeAction(_ sender: UISwipeGestureRecognizer) {
     fetchAndShowDataFor(cities.randomElement()!)
   }
 
-  func fetchAndShowDataFor(_ city: String) {
+  private func fetchAndShowDataFor(_ city: String) {
     currentCity = city
-    getCurrentTemperatureCelsius(cityName: city, callback: {
+    getCurrentTemperatureCelsius(cityName: city, callback: { [weak self] in
+      guard let self = self else {
+        return
+      }
+
       self.temperatureTextField.text = "\(Int($0.main.temp))°c"
       self.cityNameTextField.text = city
       self.weatherIconTextField.text = String ($0.weather.first!.main)
     })
   }
 
-  func getCurrentTemperatureCelsius(cityName: String, callback: @escaping (WeatherResponse) -> Void) {
+  private func getCurrentTemperatureCelsius(cityName: String, callback: @escaping (WeatherResponse) -> Void) {
     Alamofire.request(
       "https://api.openweathermap.org/data/2.5/weather",
       parameters: [
@@ -70,8 +62,18 @@ class ViewController: UIViewController {
       do {
       let weatherData = try JSONDecoder().decode(WeatherResponse.self, from: response.data!)
       callback(weatherData)
-      } catch let error {
-        print("error: \(error)")
+      } catch {
+        let alert = UIAlertController(
+          title: "Error",
+          message: "Error receiving data from OWM",
+          preferredStyle: UIAlertController.Style.alert
+        )
+        alert.addAction(UIAlertAction(
+          title: "Retry",
+          style: UIAlertAction.Style.default,
+          handler: {_ in self.fetchAndShowDataFor(self.currentCity!)}
+        ))
+        self.present(alert, animated: true, completion: nil)
       }
     }
   }
---
+++ Weather/WeatherResponse.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherResponse.swift
+//  Weather
+//
+//  Created by Ilya Lebedev on 10/08/2019.
+//  Copyright © 2019 Ilya Lebedev. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherResponse: Decodable {
+  struct MainWeather: Decodable {
+    let temp: Double
+  }
+  struct Weather: Decodable {
+    let main: String
+  }
+  let coord: [String: Double]
+  let main: MainWeather
+  let weather: [Weather]
+}
