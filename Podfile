b5c2de365ade22cf08495ee9570147bfced162b8
Илья Лебедев <i.lebedev@bestdoctor.ru>
2019-08-10 11:51:08 +0300

    Add base weather visualisation

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+use_frameworks!
+
+target 'Weather' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Alamofire', '~> 4.7'
+  pod 'SwiftLint', '~> 0.34'
+
+  target 'WeatherTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'WeatherUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -7,14 +7,72 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+struct WeatherResponse: Decodable {
+  struct MainWeather: Decodable {
+    let temp: Double
+  }
+  struct Weather: Decodable {
+    let main: String
+  }
+  let coord: [String: Double]
+  let main: MainWeather
+  let weather: [Weather]
 }
 
+class ViewController: UIViewController {
+  @IBOutlet weak var temperatureTextField: UITextField!
+  @IBOutlet weak var cityNameTextField: UITextField!
+  @IBOutlet weak var weatherIconTextField: UITextField!
+  @IBOutlet weak var scrollView: UIScrollView!
+
+  let cities = ["Moscow", "London", "Paris", "New-York"]
+  var currentCity: String?
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    fetchAndShowDataFor(cities.randomElement()!)
+
+    let refreshControl = UIRefreshControl()
+    refreshControl.addTarget(self, action: #selector(updateWeatherData), for: .valueChanged)
+    scrollView.refreshControl = refreshControl
+  }
+
+  @objc func updateWeatherData(refreshControl: UIRefreshControl) {
+    fetchAndShowDataFor(currentCity!)
+    refreshControl.endRefreshing()
+  }
+
+  @IBAction func swipeAction(_ sender: UISwipeGestureRecognizer) {
+    fetchAndShowDataFor(cities.randomElement()!)
+  }
+
+  func fetchAndShowDataFor(_ city: String) {
+    currentCity = city
+    getCurrentTemperatureCelsius(cityName: city, callback: {
+      self.temperatureTextField.text = "\(Int($0.main.temp))°c"
+      self.cityNameTextField.text = city
+      self.weatherIconTextField.text = String ($0.weather.first!.main)
+    })
+  }
+
+  func getCurrentTemperatureCelsius(cityName: String, callback: @escaping (WeatherResponse) -> Void) {
+    Alamofire.request(
+      "https://api.openweathermap.org/data/2.5/weather",
+      parameters: [
+        "q": cityName,
+        "appid": Bundle.main.infoDictionary?["OWM_API_KEY"] as! String,  // swiftlint:disable:this force_cast
+        "units": "Metric"
+      ]
+    ).responseJSON { response in
+      do {
+      let weatherData = try JSONDecoder().decode(WeatherResponse.self, from: response.data!)
+      callback(weatherData)
+      } catch let error {
+        print("error: \(error)")
+      }
+    }
+  }
+}
